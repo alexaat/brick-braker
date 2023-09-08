0f8145d2552715b5bab89168a858682cdd39b496
alexaat <alexaat@yahoo.co.uk>
2023-09-08 19:10:05 +0100

    Update constants.js

--- scripts/constants.js
+++ scripts/constants.js
@@ -1,5 +1,6 @@
 export const playBoardWidth = 12;
 export const playBoardHeight = 20;
 export const blockSize = 35;
-export const paddleWidth = 110;
-export const paddleHeight = 30;
+export const paddleWidth = 140;
+export const paddleHeight = 36;
+export const ballSize = 16;
