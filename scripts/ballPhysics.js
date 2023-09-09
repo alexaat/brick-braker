cd2b18ba27fbc26b09bea2467d9da2ba51b370be
aliakseividaseu <alexaat@yahoo.co.uk>
2023-09-09 23:11:27 +0100

    Collision left wall

--- scripts/ballPhysics.js
+++ scripts/ballPhysics.js
@@ -2,22 +2,32 @@ import { defaultSpeedX, defaultSpeedY } from "./constants.js";
 
 let speedX = defaultSpeedX;
 let speedY = defaultSpeedY;
-
-
 const ball = document.querySelector('#ball');
-
-
+const playBoard = document.querySelector('#board');
+const rect = playBoard.getBoundingClientRect();
 
 export const updateBallPosition = () => {
  
     const x = parseInt(ball.style.left.replace('px',''));
     const y = parseInt(ball.style.top.replace('px',''));
 
+    let x1 = x + speedX;
+    let y1 = y + speedY;
 
-    ball.style.left = `${x + speedX}px`;
-    ball.style.top = `${y + speedY}px`;
-    //console.log(rect.top)
+    x1, y1 = checkCollisions(x1, y1)
+
+    ball.style.left = `${x1}px`;
+    ball.style.top = `${y1}px`;
+}
 
 
 
+
+
+const checkCollisions = (x1, y1) => {
+
+    if (x1<0) {
+        speedX = !speedX;
+    }
+    return x1, y1;
 }
