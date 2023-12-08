0aaf3a1253a0cd4219643b86cba81aa2d671438b
avidaseu <alexaat@yahoo.co.uk>
2023-12-08 15:49:08 -0800

    level3 map

--- levels/level1.js
+++ levels/level1.js
@@ -1,5 +1,5 @@
-export const level1 = [      
-    {         
+export const level1 = [
+    {
         id: 1,
         left: 0,
         top: 0,
@@ -7,9 +7,9 @@ export const level1 = [
         height: 15,
         hits: 1,
         type: 'purple_brick'
-     },
-     
-    { 
+    },
+
+    {
         id: 2,
         left: 50,
         top: 0,
@@ -17,8 +17,8 @@ export const level1 = [
         height: 15,
         hits: 1,
         type: 'purple_brick'
-      },
-    { 
+    },
+    {
         id: 3,
         left: 100,
         top: 0,
@@ -27,7 +27,7 @@ export const level1 = [
         hits: 1,
         type: 'purple_brick'
     },
-    { 
+    {
         id: 4,
         left: 150,
         top: 0,
@@ -36,7 +36,7 @@ export const level1 = [
         hits: 1,
         type: 'purple_brick'
     },
-    { 
+    {
         id: 5,
         left: 200,
         top: 0,
@@ -45,7 +45,7 @@ export const level1 = [
         hits: 1,
         type: 'purple_brick'
     },
-    { 
+    {
         id: 6,
         left: 250,
         top: 0,
@@ -54,7 +54,7 @@ export const level1 = [
         hits: 1,
         type: 'purple_brick'
     },
-    { 
+    {
         id: 7,
         left: 0,
         top: 15,
@@ -62,8 +62,8 @@ export const level1 = [
         height: 15,
         hits: 1,
         type: 'red_brick'
-     },
-    { 
+    },
+    {
         id: 8,
         left: 50,
         top: 15,
@@ -72,7 +72,7 @@ export const level1 = [
         hits: 1,
         type: 'red_brick'
     },
-    { 
+    {
         id: 9,
         left: 100,
         top: 15,
@@ -81,7 +81,7 @@ export const level1 = [
         hits: 1,
         type: 'red_brick'
     },
-    { 
+    {
         id: 10,
         left: 150,
         top: 15,
@@ -90,7 +90,7 @@ export const level1 = [
         hits: 1,
         type: 'red_brick'
     },
-    { 
+    {
         id: 11,
         left: 200,
         top: 15,
@@ -99,7 +99,7 @@ export const level1 = [
         hits: 1,
         type: 'red_brick'
     },
-    { 
+    {
         id: 12,
         left: 250,
         top: 15,
@@ -108,7 +108,7 @@ export const level1 = [
         hits: 1,
         type: 'red_brick'
     },
-    { 
+    {
         id: 13,
         left: 0,
         top: 30,
@@ -116,8 +116,8 @@ export const level1 = [
         height: 15,
         hits: 1,
         type: 'yellow_brick'
-     },
-    { 
+    },
+    {
         id: 14,
         left: 50,
         top: 30,
@@ -126,8 +126,8 @@ export const level1 = [
         hits: 1,
         type: 'yellow_brick'
     },
-    
-    { 
+
+    {
         id: 15,
         left: 100,
         top: 30,
@@ -135,9 +135,9 @@ export const level1 = [
         height: 15,
         hits: 1,
         type: 'yellow_brick'
-    },  
-    
-    { 
+    },
+
+    {
         id: 16,
         left: 150,
         top: 30,
@@ -146,7 +146,7 @@ export const level1 = [
         hits: 1,
         type: 'yellow_brick'
     },
-    { 
+    {
         id: 17,
         left: 200,
         top: 30,
@@ -155,7 +155,7 @@ export const level1 = [
         hits: 1,
         type: 'yellow_brick'
     },
-    { 
+    {
         id: 18,
         left: 250,
         top: 30,
@@ -163,9 +163,9 @@ export const level1 = [
         height: 15,
         hits: 1,
         type: 'yellow_brick'
-    }, 
-     
-    { 
+    },
+
+    {
         id: 19,
         left: 60,
         top: 190,
@@ -173,7 +173,7 @@ export const level1 = [
         height: 25,
         type: 'blue_block'
     },
-    { 
+    {
         id: 20,
         left: 200,
         top: 190,
@@ -181,20 +181,22 @@ export const level1 = [
         height: 25,
         type: 'blue_block'
     },
-    { 
+    {
         id: 21,
         left: 10,
         top: 315,
         width: 25,
         height: 25,
         type: 'blue_block'
-     },
-     { 
+    },
+    {
         id: 22,
         left: 250,
         top: 315,
         width: 25,
         height: 25,
         type: 'blue_block'
-     },   
+    },
+
+
 ];
---
+++ levels/level3.js
@@ -0,0 +1,518 @@
+export const level3 = [
+
+    //Level3
+
+    //1
+    {
+        id: 1,
+        left: 225,
+        top: 25,
+        width: 50,
+        height: 15,
+        hits: 1,
+        type: 'purple_brick'
+    },
+    {
+        id: 2,
+        left: 225,
+        top: 40,
+        width: 50,
+        height: 15,
+        hits: 1,
+        type: 'purple_brick'
+    },
+    {
+        id: 3,
+        left: 225,
+        top: 55,
+        width: 50,
+        height: 15,
+        hits: 1,
+        type: 'purple_brick'
+    },
+    {
+        id: 4,
+        left: 225,
+        top: 70,
+        width: 50,
+        height: 15,
+        hits: 1,
+        type: 'purple_brick'
+    },
+    {
+        id: 5,
+        left: 225,
+        top: 85,
+        width: 50,
+        height: 15,
+        hits: 1,
+        type: 'purple_brick'
+    },
+    {
+        id: 6,
+        left: 225,
+        top: 100,
+        width: 50,
+        height: 15,
+        hits: 1,
+        type: 'purple_brick'
+    },
+    {
+        id: 8,
+        left: 225,
+        top: 115,
+        width: 50,
+        height: 15,
+        hits: 1,
+        type: 'purple_brick'
+    },
+    {
+        id: 9,
+        left: 225,
+        top: 130,
+        width: 50,
+        height: 15,
+        hits: 1,
+        type: 'purple_brick'
+    },
+    {
+        id: 10,
+        left: 225,
+        top: 145,
+        width: 50,
+        height: 15,
+        hits: 1,
+        type: 'purple_brick'
+    },
+    {
+        id: 43,
+        left: 225,
+        top: 160,
+        width: 50,
+        height: 15,
+        hits: 1,
+        type: 'purple_brick'
+    },
+    {
+        id: 44,
+        left: 225,
+        top: 175,
+        width: 50,
+        height: 15,
+        hits: 1,
+        type: 'purple_brick'
+    },
+    {
+        id: 45,
+        left: 225,
+        top: 190,
+        width: 50,
+        height: 15,
+        hits: 1,
+        type: 'purple_brick'
+    },
+    {
+        id: 46,
+        left: 225,
+        top: 205,
+        width: 50,
+        height: 15,
+        hits: 1,
+        type: 'purple_brick'
+    },
+    {
+        id: 47,
+        left: 225,
+        top: 220,
+        width: 50,
+        height: 15,
+        hits: 1,
+        type: 'purple_brick'
+    },
+    {
+        id: 48,
+        left: 225,
+        top: 235,
+        width: 50,
+        height: 15,
+        hits: 1,
+        type: 'purple_brick'
+    },
+    {
+        id: 49,
+        left: 225,
+        top: 250,
+        width: 50,
+        height: 15,
+        hits: 1,
+        type: 'purple_brick'
+    },
+    {
+        id: 50,
+        left: 225,
+        top: 265,
+        width: 50,
+        height: 15,
+        hits: 1,
+        type: 'purple_brick'
+    },
+
+    //0
+    {
+        id: 11,
+        left: 85,
+        top: 25,
+        width: 50,
+        height: 15,
+        hits: 1,
+        type: 'purple_brick'
+    },
+    {
+        id: 12,
+        left: 45,
+        top: 40,
+        width: 50,
+        height: 15,
+        hits: 1,
+        type: 'purple_brick'
+    },
+    {
+        id: 13,
+        left: 125,
+        top: 40,
+        width: 50,
+        height: 15,
+        hits: 1,
+        type: 'purple_brick'
+    },
+    {
+        id: 14,
+        left: 25,
+        top: 55,
+        width: 50,
+        height: 15,
+        hits: 1,
+        type: 'purple_brick'
+    },
+    {
+        id: 15,
+        left: 145,
+        top: 55,
+        width: 50,
+        height: 15,
+        hits: 1,
+        type: 'purple_brick'
+    },
+    {
+        id: 16,
+        left: 25,
+        top: 70,
+        width: 50,
+        height: 15,
+        hits: 1,
+        type: 'purple_brick'
+    },
+    {
+        id: 17,
+        left: 145,
+        top: 70,
+        width: 50,
+        height: 15,
+        hits: 1,
+        type: 'purple_brick'
+    },
+    {
+        id: 18,
+        left: 25,
+        top: 85,
+        width: 50,
+        height: 15,
+        hits: 1,
+        type: 'purple_brick'
+    },
+    {
+        id: 19,
+        left: 145,
+        top: 85,
+        width: 50,
+        height: 15,
+        hits: 1,
+        type: 'purple_brick'
+    },
+    {
+        id: 20,
+        left: 25,
+        top: 100,
+        width: 50,
+        height: 15,
+        hits: 1,
+        type: 'purple_brick'
+    },
+    {
+        id: 21,
+        left: 145,
+        top: 100,
+        width: 50,
+        height: 15,
+        hits: 1,
+        type: 'purple_brick'
+    },
+    {
+        id: 22,
+        left: 25,
+        top: 115,
+        width: 50,
+        height: 15,
+        hits: 1,
+        type: 'purple_brick'
+    },
+    {
+        id: 23,
+        left: 145,
+        top: 115,
+        width: 50,
+        height: 15,
+        hits: 1,
+        type: 'purple_brick'
+    },
+    {
+        id: 24,
+        left: 25,
+        top: 130,
+        width: 50,
+        height: 15,
+        hits: 1,
+        type: 'purple_brick'
+    },
+    {
+        id: 25,
+        left: 145,
+        top: 130,
+        width: 50,
+        height: 15,
+        hits: 1,
+        type: 'purple_brick'
+    },
+    {
+        id: 26,
+        left: 25,
+        top: 145,
+        width: 50,
+        height: 15,
+        hits: 1,
+        type: 'purple_brick'
+    },
+    {
+        id: 27,
+        left: 145,
+        top: 145,
+        width: 50,
+        height: 15,
+        hits: 1,
+        type: 'purple_brick'
+    },
+    {
+        id: 28,
+        left: 25,
+        top: 160,
+        width: 50,
+        height: 15,
+        hits: 1,
+        type: 'purple_brick'
+    },
+    {
+        id: 29,
+        left: 145,
+        top: 160,
+        width: 50,
+        height: 15,
+        hits: 1,
+        type: 'purple_brick'
+    },
+    {
+        id: 30,
+        left: 25,
+        top: 175,
+        width: 50,
+        height: 15,
+        hits: 1,
+        type: 'purple_brick'
+    },
+    {
+        id: 31,
+        left: 145,
+        top: 175,
+        width: 50,
+        height: 15,
+        hits: 1,
+        type: 'purple_brick'
+    },
+    {
+        id: 32,
+        left: 25,
+        top: 190,
+        width: 50,
+        height: 15,
+        hits: 1,
+        type: 'purple_brick'
+    },
+    {
+        id: 33,
+        left: 145,
+        top: 190,
+        width: 50,
+        height: 15,
+        hits: 1,
+        type: 'purple_brick'
+    },
+    {
+        id: 34,
+        left: 25,
+        top: 205,
+        width: 50,
+        height: 15,
+        hits: 1,
+        type: 'purple_brick'
+    },
+    {
+        id: 35,
+        left: 145,
+        top: 205,
+        width: 50,
+        height: 15,
+        hits: 1,
+        type: 'purple_brick'
+    },
+    {
+        id: 36,
+        left: 25,
+        top: 220,
+        width: 50,
+        height: 15,
+        hits: 1,
+        type: 'purple_brick'
+    },
+    {
+        id: 37,
+        left: 145,
+        top: 220,
+        width: 50,
+        height: 15,
+        hits: 1,
+        type: 'purple_brick'
+    },
+    {
+        id: 38,
+        left: 25,
+        top: 235,
+        width: 50,
+        height: 15,
+        hits: 1,
+        type: 'purple_brick'
+    },
+    {
+        id: 39,
+        left: 145,
+        top: 235,
+        width: 50,
+        height: 15,
+        hits: 1,
+        type: 'purple_brick'
+    },
+    {
+        id: 40,
+        left: 45,
+        top: 250,
+        width: 50,
+        height: 15,
+        hits: 1,
+        type: 'purple_brick'
+    },
+    {
+        id: 41,
+        left: 125,
+        top: 250,
+        width: 50,
+        height: 15,
+        hits: 1,
+        type: 'purple_brick'
+    },
+    {
+        id: 42,
+        left: 85,
+        top: 265,
+        width: 50,
+        height: 15,
+        hits: 1,
+        type: 'purple_brick'
+    },
+
+    //Blocks
+    {
+        id: 51,
+        left: 25,
+        top: 315,
+        width: 25,
+        height: 25,
+        type: 'blue_block'
+    },
+    {
+        id: 52,
+        left: 49,
+        top: 300,
+        width: 25,
+        height: 25,
+        type: 'blue_block'
+    },
+    {
+        id: 53,
+        left: 73,
+        top: 315,
+        width: 25,
+        height: 25,
+        type: 'blue_block'
+    },
+    {
+        id: 54,
+        left: 97,
+        top: 300,
+        width: 25,
+        height: 25,
+        type: 'blue_block'
+    },
+
+    {
+        id: 56,
+        left: 250,
+        top: 300,
+        width: 25,
+        height: 25,
+        type: 'blue_block'
+    },
+    {
+        id: 57,
+        left: 226,
+        top: 315,
+        width: 25,
+        height: 25,
+        type: 'blue_block'
+    },
+    {
+        id: 58,
+        left: 202,
+        top: 300,
+        width: 25,
+        height: 25,
+        type: 'blue_block'
+    },
+    {
+        id: 59,
+        left: 178,
+        top: 315,
+        width: 25,
+        height: 25,
+        type: 'blue_block'
+    },
+
+
+];
