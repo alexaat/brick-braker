b2763be20f7316c00eea02ca1174037b9ec2b9a1
avidaseu <alexaat@yahoo.co.uk>
2024-04-10 18:19:42 +0100

    constants update

--- scripts/constants.js
+++ scripts/constants.js
@@ -26,6 +26,7 @@ export const paddleImagesSource = ['paddle_0.jpg', 'paddle_1.jpg', 'paddle_2.jpg
 
 export const allowedCharacters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 -Backspace';
 
-export const endpoint = 'http://localhost:8080';
+// export const endpoint = 'http://localhost:8080';
+export const endpoint = 'http://16.171.27.226:8080';
 export const maxItemsPerPage = 23;
 export const maxNameLength = 12;
